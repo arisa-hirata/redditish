5699644f190aecc9031a7632403a04a5496a4da0
“arisa-hirata” <arisa.pirata@gmail.com>
2020-12-18 21:28:09 -0800

    added forget password for server

--- server/dist/constants.js
+++ server/dist/constants.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.COOKIE_NAME = exports.__prod__ = void 0;
+exports.FORGET_PASSWORD_PREFEX = exports.COOKIE_NAME = exports.__prod__ = void 0;
 exports.__prod__ = process.env.NODE_ENV === 'production';
 exports.COOKIE_NAME = "qid";
+exports.FORGET_PASSWORD_PREFEX = "forget-password";
 //# sourceMappingURL=constants.js.map
