3f6dd353143bda9e4c485de29422a96b409e2bad
“arisa-hirata” <arisa.pirata@gmail.com>
2020-12-21 14:18:50 -0800

    added pagenation  using URQL

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,13 +4,18 @@ import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
 import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import NextLink from "next/link";
-import React from "react";
+import { useState } from "react";
 
 const Index = () => {
+    const [variables, setVariables] = useState({
+        limit: 10,
+        cursor: null as null | string,
+    });
+
+    console.log(variables);
+
     const [{ data, fetching }] = usePostsQuery({
-        variables: {
-            limit: 10,
-        },
+        variables
     });
 
     if (!fetching && !data) {
@@ -40,7 +45,17 @@ const Index = () => {
                 )}
             {data ? (
                 <Flex>
-                    <Button isLoading={fetching} m="auto" my={8}>
+                    <Button
+                        onClick={() => {
+                            setVariables({
+                                limit: variables.limit,
+                                cursor: data.posts[data.posts.length - 1].createdAt
+                            });
+                    }}
+                        isLoading={fetching}
+                        m="auto"
+                        my={8}
+                    >
                         Load more
                     </Button>
                 </Flex>
